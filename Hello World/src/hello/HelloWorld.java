d341f6b9f24f432932c2a43877b7ad4af9b0559f
peter.geertsema <peter.geertsema@NESTENE.corp.inflectra.com>
2017-06-27 14:55:16 -0400

    Adding more files

--- Hello World/src/hello/HelloWorld.java
+++ Hello World/src/hello/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("I can just keep on adding!");
 		
-		
+		//this does nothing
 		print();
 
 	}
---
+++ Hello World/src/hello/WhyNot.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class WhyNot {
+
+	public static void main(String[] args) {
+		System.out.println("I love git!");
+	}
+
+}
