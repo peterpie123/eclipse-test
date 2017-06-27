fb64ae20970606ac41248ac5fab2a08ddddff795
peter.geertsema <peter.geertsema@NESTENE.corp.inflectra.com>
2017-06-27 14:53:54 -0400

    Added more friendliness to saying hello

--- Hello World/src/hello/HelloWorld.java
+++ Hello World/src/hello/HelloWorld.java
@@ -4,7 +4,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("I can just keep on adding!");
+		
+		
+		print();
 
+	}
+	
+	private static void print() {
+		System.out.println("I can even add methods!");
 	}
 
 }
