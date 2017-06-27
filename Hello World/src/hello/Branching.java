6f35d984c472dc4d142a1b4e86b5797572c5c5de
peter.geertsema <iwannapjg@gmail.com>
2017-06-27 15:10:52 -0400

    Trying another commit so that I can do a pull request

--- Hello World/src/hello/Branching.java
+++ Hello World/src/hello/Branching.java
@@ -3,6 +3,6 @@ package hello;
 public class Branching {
 	public static void main(String[] args){
 		System.out.println("Is this on a different branch?");
-		//I hope that it's not on master
+		//I hope this gets put on master after a pull request
 	}
 }
